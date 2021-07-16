2eb4d9b36d9e89939681dc1035bf3496f59403ba
itsgracian <gracian2020@gmail.com>
2021-07-16 16:34:11 +0200

    set text in center

--- src/component/CustomEditor/CustomEditor.tsx
+++ src/component/CustomEditor/CustomEditor.tsx
@@ -19,21 +19,34 @@ library.add(faParagraph);
 library.add(faLink);
 library.add(faCode);
 
-const CustomEditor = () => {
+interface Props {
+  height?: string;
+}
+
+const CustomEditor = (props: Props) => {
   const [editorState, setEditorState] = useState<EditorState>(
     EditorState.createEmpty()
   );
+
+  const { height } = props;
 
   const handleOnChange = (value: EditorState) => {
     setEditorState(value);
   };
 
   return (
-    <div className='customTextEditor relative w-full h-full flex flex-col items-center justify-center'>
+    <div
+      className='customTextEditor relative w-full h-full flex flex-col items-center justify-center'
+      style={{ height: height || '500px' }}
+    >
       <EditorAction
         items={['heading', 'paragraph', 'bold', 'italic', 'link', 'code']}
       />
-      <div className='editorContent relative w-full flex-1'>
+      <div
+        className={`editorContent relative w-full flex-1 mt-8 ${
+          editorState ? 'text-center' : ''
+        }`}
+      >
         <Editor
           editorState={editorState}
           onChange={handleOnChange}
